455a0c79b763be4583eb13952aad7c3774c227ae
Reydner Miranda <reydnermn@hotmail.com>
2023-06-14 23:24:18 -0300

    Implementação da movimentação e ataque do player

---
+++ Assets/2- Scripts/1-Player/JumpingAttackPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingAttackPlayer : MonoBehaviour
+{
+    //Script de reconhecimento de ataque do personagem
+
+    [SerializeField] private BoxCollider2D colliderAtkPlayer;
+
+    void Start()
+    {
+        colliderAtkPlayer = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        //inverção da posição do colisor de ataque baseado na posição do player
+        if (PlayerMovement2D.move < 0)
+        {
+            colliderAtkPlayer.offset = new Vector2(-1f, 0);
+        }
+        else if (PlayerMovement2D.move > 0)
+        {
+            colliderAtkPlayer.offset = new Vector2(1f, 0);
+        }
+    }
+}
+
