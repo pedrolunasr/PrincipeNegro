48809661e8c0c00b10383e3bffde2d229980d9c4
Thiago Silva <thiagosantos346@gmail.com>
2023-07-06 22:14:56 -0300

    Ajustado posicionamento da camera para não deixar o pulo estranho.

--- Assets/2- Scripts/2- Camera/CameraFollow.cs
+++ Assets/2- Scripts/2- Camera/CameraFollow.cs
@@ -7,17 +7,19 @@ public class CameraFollow : MonoBehaviour
 
     public Transform target;
     public float smoothSpeed = 0.9f;
+    public float initialPosition;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        initialPosition = target.position.y + 0.99f;
         
     }
 
 
     void FixedUpdate()
     {
-        Vector3 startPosition = new Vector3(target.position.x, target.position.y + 0.45f, -1f);
+        Vector3 startPosition = new Vector3(target.position.x, initialPosition, -1f);
         Vector3 smoothPosition = Vector3.Lerp(transform.position, startPosition, smoothSpeed);
         transform.position = smoothPosition;
     }
