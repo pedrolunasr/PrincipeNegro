2824309e5ee65c71db59d3cdb429c475ddf97e84
Thiago Silva <thiagosantos346@gmail.com>
2023-08-13 16:18:47 -0300

    add efects sounds

---
+++ Assets/2- Scripts/0-Refactor/Gameplay/EnemyAttack.cs
@@ -0,0 +1,37 @@
+﻿using Platformer.Core;
+using Platformer.Mechanics;
+using UnityEngine;
+
+namespace Platformer.Gameplay
+{
+    /// <summary>
+    /// Attack when the player collider
+    /// </summary>
+    /// <typeparam name="EnemyAttack"></typeparam>
+    public class EnemyAttack : Simulation.Event<EnemyAttack>
+    {
+        public EnemyController enemy;
+
+        public override void Execute()
+        {
+            enemy._collider.enabled = false;
+            enemy.control.enabled = false;
+
+            AttackCheckSimpleWarrior.checkAttack = false;
+
+            enemy._animator.SetTrigger("Attack");
+            //moveSpeed = 0;
+
+            //yield return new WaitForSeconds(1.5f); //Velocidade de ataque/recupera��o
+
+            //moveSpeed = 1.2f;
+            //colliderCheckAtk.enabled = true;
+
+            enemy._collider.enabled = true;
+            enemy.control.enabled = true;
+
+            if (enemy._audio && enemy.ouch)
+                enemy._audio.PlayOneShot(enemy.ouch);
+        }
+    }
+}
